807eb9dcd5f7a846d44817896bb205c517848bf4
sundeep-co-in <suanand@redhat.com>
2018-08-24 14:59:27 +0530

    rsvp states

--- rsvp/__init__.py
+++ rsvp/__init__.py
@@ -11,6 +11,8 @@ class RSVP(object):
     RSVP Main Class
     """
 
+    MEMBERS_KEY = 'event_members'
+
     def __init__(self):
         """
         constructor
@@ -59,7 +61,7 @@ class RSVP(object):
         event_master_dict['event_description'] = event_details.get('event_description')
         event_master_dict['event_start_date'] = str(event_details['event_start_date'])
         event_master_dict['event_end_date'] = str(event_details.get('event_end_date'))
-        event_master_dict['event_members'] = event_details.get('event_members', [])
+        event_master_dict[self.MEMBERS_KEY] = event_details.get('event_members', [])
 
         self.store_file_name = str(event_id) + '.' + event_details['event_slug']
         if self.helpers.save_rsvp(self.store_file_name, event_master_dict):
@@ -88,7 +90,47 @@ class RSVP(object):
         event_data = self.helpers.load_file_data(file_to_append)
         member_keys_dict = {key: False for key in MEMBER_KEYS}
         members_list = [{member: member_keys_dict} for member in members]
-        event_data['event_members'].extend(members_list)
+        event_data[self.MEMBERS_KEY].extend(members_list)
         if self.helpers.save_rsvp(self.store_file_name, event_data):
             return True
         return False
+
+    def adjust_rsvp_state(self, event_id, members, **states):
+        """
+        Set rsvp for members for an event
+        :param event_id: uuid as string
+        :param members: list
+        :param states: dict example {'in_queue_waiting': true}
+        :return: boolean
+        """
+        file_to_update = self._locate_file(event_id)
+        event_data = self.helpers.load_file_data(file_to_update)
+
+        for member_rsvp in event_data.get(self.MEMBERS_KEY, []):
+            member = [m for m in members if member_rsvp.get(m)]
+            if member and len(member) == 1:
+                member = member[0]
+                new_state = {i: states.get(i, j) for i, j in member_rsvp[member].items()}
+                member_index = event_data[self.MEMBERS_KEY].index(member_rsvp)
+                event_data[self.MEMBERS_KEY].pop(member_index)
+                event_data[self.MEMBERS_KEY].insert(
+                    member_index, {member: new_state}
+                )
+        if self.helpers.save_rsvp(file_to_update, event_data):
+            return True
+        return False
+
+    def get_rsvp_state(self, event_id, members):
+        """
+        Get rsvp for members for an event
+        :param event_id: uuid as string
+        :param members: list
+        :return: list of dict
+        """
+        members_rsvp = []
+        file_to_update = self._locate_file(event_id)
+        event_data = self.helpers.load_file_data(file_to_update)
+        members_data = event_data.get(self.MEMBERS_KEY, [])
+        if members_data:
+            [members_rsvp.append(member) for member in members_data if list(member)[0] in members]
+        return members_rsvp
--- test_rsvp.py
+++ test_rsvp.py
@@ -5,6 +5,7 @@ import unittest
 from datetime import date
 from uuid import UUID
 
+from rsvp.constants import MEMBER_KEYS
 from rsvp import RSVP
 
 
@@ -44,6 +45,53 @@ class TestRSVP(unittest.TestCase):
 
         add_result = self.rsvp_object.add_members(self.new_event_rsvp, rsvp_members)
         self.assertTrue(add_result)
+
+    def test_adjust_rsvp_state(self):
+        """
+        test adjust_rsvp_state
+        """
+        rsvp_members = ['Rahul', 'Shyam', 'Anu', 'Isha']
+        add_result = self.rsvp_object.add_members(self.new_event_rsvp, rsvp_members)
+
+        if add_result:
+            rsvp_members_to_update = ['Rahul', 'Shyam']
+            state1 = {MEMBER_KEYS[0]: True}
+
+            update_result_1 = self.rsvp_object.adjust_rsvp_state(
+                self.new_event_rsvp, rsvp_members_to_update, **state1
+            )
+            self.assertTrue(update_result_1)
+
+            state2 = {MEMBER_KEYS[1]: True}
+            update_result_2 = self.rsvp_object.adjust_rsvp_state(
+                self.new_event_rsvp, rsvp_members_to_update, **state2
+            )
+            self.assertTrue(update_result_2)
+
+    def test_get_rsvp_state(self):
+        """
+        test get_rsvp_state
+        """
+        rsvp_members = ['Rahul', 'Shyam', 'Anu', 'Isha']
+        add_result = self.rsvp_object.add_members(self.new_event_rsvp, rsvp_members)
+
+        if add_result:
+            rsvp_members_to_update = ['Rahul', 'Shyam']
+            state1 = {MEMBER_KEYS[0]: True, MEMBER_KEYS[1]: True, MEMBER_KEYS[2]: True}
+
+            update_result_1 = self.rsvp_object.adjust_rsvp_state(
+                self.new_event_rsvp, rsvp_members_to_update, **state1
+            )
+            self.assertTrue(update_result_1)
+
+            result = self.rsvp_object.get_rsvp_state(
+                self.new_event_rsvp, [rsvp_members_to_update[0]]
+            )
+            self.assertIsInstance(result, list)
+            self.assertDictEqual(
+                result[0], {'Rahul': {'confirm_for_seat': True,
+                                      'attended_event': True, 'in_queue_waiting': True}}
+            )
 
 
 def rsvp_test_suit():
